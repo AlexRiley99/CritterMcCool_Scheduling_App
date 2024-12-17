e25ec03166f6106ebdf9fe28b804b343ea2504ea
AlexRiley99 <alexriley1099@gmail.com>
2024-12-17 13:00:13 -0500

    Added navigation from Confirmation page, back to the Main page.

--- app/src/main/java/com/example/crittermccoolscheduling/Confirmation.kt
+++ app/src/main/java/com/example/crittermccoolscheduling/Confirmation.kt
@@ -1,6 +1,8 @@
 package com.example.crittermccoolscheduling
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class Confirmation : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //Handler for the "Ok" button
+        val Ok_Btn: Button = findViewById(R.id.Ok_Btn)
+        Ok_Btn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
